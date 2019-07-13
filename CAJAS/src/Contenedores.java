46a4de87314362767a54f16da6004cf83e234dac
Morales Javier <48968846+javifandejaz@users.noreply.github.com>
2019-07-13 12:52:40 -0300

    Add files via upload

---
+++ CAJAS/src/Contenedores.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+
+public class Contenedores {
+
+	public static void main(String[] args) {
+		
+		String detalle=JOptionPane.showInputDialog("Introduce el detalle de referencia");
+		
+		System.out.print(detalle);		
+	}
+
+}
